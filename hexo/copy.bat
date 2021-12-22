ec24fef8a7b2f0ef0b19962461a4105438361328
lvxingzhi <miaocheng2021>
2021-12-22 15:49:01 +0800

    init

---
+++ hexo/copy.bat
@@ -0,0 +1,3 @@
+XCOPY /e /y C:\work\workspace\data-space\hexo\note C:\work\soft\hexo\source\_posts /D:02-21-2012
+XCOPY /e /y C:\work\workspace\data-space\hexo\images C:\work\soft\hexo\source\images /D:02-21-2012
+pause
